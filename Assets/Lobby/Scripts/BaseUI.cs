705b6c161d32033c22b6358903d662ab26913c43
YoungsonMan <choi13570@naver.com>
2024-11-16 21:53:23 +0900

    Chore : Move to Mac

--- Assets/Lobby/Scripts/BaseUI.cs
+++ Assets/Lobby/Scripts/BaseUI.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class BaseUI : MonoBehaviour
 {
+    private Dictionary<string, GameObject> gameObjectDic;
+    private Dictionary<string, Component> componentDic;
     protected virtual void Awake()
     {
         Bind();
@@ -12,18 +14,28 @@ public class BaseUI : MonoBehaviour
 
     private void Bind()
     {
-
+        Transform[] transforms = GetComponentsInChildren<Transform>(true);
+        gameObjectDic = new Dictionary<string, GameObject>(transforms.Length << 2);
+        foreach (Transform child in transforms)
+        {
+            gameObjectDic.TryAdd(child.gameObject.name, child.gameObject);
+        }
     }
 
     // 이름이 name인 UI 게임오브젝트 가져오기
-   // public GameObject GetUI(in string name)
-   // {
-   //
-   // }
-   //
+    // GetUI("Key") : Key 이름의 게임오브젝트 가져오기
+    public GameObject GetUI(in string name)
+    {
+        gameObjectDic.TryGetValue(name, out GameObject gameObject);
+        return gameObject;
+   
+    }
+   
+    // 이름이 name인 UI에서 컴포넌트 T 가져오기
+    // GetUI<Image>("Key") : Import Image Component name of Key from the GameObject.
    // public T GetUI<T>(in string name) where T : Component
    // {
-   //
+   
    // }
-   //
+   
 }
